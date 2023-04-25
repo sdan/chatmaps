bc749214eecc2cb8a0547b0a10cc5e90812718ce
Surya <git@sdan.io>
2023-04-24 23:55:45 -0700

    before refactor

--- cli.py
+++ cli.py
@@ -1,59 +1,44 @@
 import argparse
 import json
-import openai
-from tenacity import retry, wait_random_exponential, stop_after_attempt
-import numpy as np
+from main import search_cafes_semantically
 import chromadb
 from chromadb.config import Settings
+from chromadb.utils import embedding_functions
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-openai.api_key = OPENAI_API_KEY
 
 client = chromadb.Client(Settings(
     anonymized_telemetry=False,
     chroma_db_impl="duckdb+parquet",
+    persist_directory="/Users/sdan/Developer/chatmaps/cafe_data",
     ))
-collection = client.get_collection(name="cafe_collection")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def load_cafe_data(file_path):
-    with open(file_path, "r") as f:
-        data = json.load(f)
-    return data
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+    api_key=OPENAI_API_KEY,
+    model_name="text-embedding-ada-002"
+)
+collection = client.get_or_create_collection(name="cafe_collection", embedding_function=openai_ef)
 
-@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
-def get_embedding(text: str, model="text-embedding-ada-002") -> list[float]:
-    response = openai.Completion.create(engine=model, prompt=f"Semantic search: {text}", max_tokens=1, n=1, stop=None, temperature=0.5)
-    return openai.Embedding.create(input=text, model=model)["data"][0]["embedding"]
-
-def cosine_similarity(a, b):
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
-
-def search_cafes_semantically(query, cafe_data, top_k=10):
-    query_embedding = get_embedding(query)
-    similarities = []
-
-    for cafe in cafe_data:
-        similarity = cosine_similarity(query_embedding, cafe["embedding"])
-        similarities.append(similarity)
-
-    top_indices = np.argsort(similarities)[-top_k:][::-1]
-    recommendations = [cafe_data[i] for i in top_indices]
-    return recommendations
 
 def main():
-    parser = argparse.ArgumentParser(description="Semantic search for cafes in San Francisco")
-    parser.add_argument("query", type=str, help="Search query, e.g., 'cafe that is around oak st that is open at 7pm'")
-    parser.add_argument("-k", "--top_k", type=int, default=10, help="Number of top results to return (default: 10)")
+    parser = argparse.ArgumentParser(description="Search for cafe recommendations based on a given query.")
+    parser.add_argument("query", type=str, help="The search query for cafe recommendations.")
+    parser.add_argument("--top_k", type=int, default=10, help="The number of top recommendations to display (default: 10).")
 
     args = parser.parse_args()
 
-    cafe_data = collection.peek()
+    # Load cafe data from the Chroma collection
+    cafe_data = collection.get(include=["metadatas"])["metadatas"][:args.top_k]
+    print(f"Cafe data: {cafe_data}")
+
+    # Search for cafe recommendations
     recommendations = search_cafes_semantically(args.query, cafe_data, args.top_k)
 
-    print("Recommended cafes:")
-    for rec in recommendations:
-        print(f"{rec.get('name', '')} - {rec.get('formatted_address', '')}")
+    # Display the recommendations
+    print(f"Top {args.top_k} cafe recommendations for the query '{args.query}':")
+    for idx, rec in enumerate(recommendations, start=1):
+        print(f"{idx}. Name: {rec['name']} \n Address: {rec['formatted_address']} \n Rating: {rec['rating']} \n")
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from chromadb.utils import embedding_functions
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 import openai
 import requests
+import os
+import numpy as np
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -15,10 +18,10 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 openai.api_key = OPENAI_API_KEY
 
-
 client = chromadb.Client(Settings(
     anonymized_telemetry=False,
     chroma_db_impl="duckdb+parquet",
+    persist_directory="/Users/sdan/Developer/chatmaps/cafe_data",
     ))
 openai_ef = embedding_functions.OpenAIEmbeddingFunction(
     api_key=OPENAI_API_KEY,
@@ -26,27 +29,58 @@ openai_ef = embedding_functions.OpenAIEmbeddingFunction(
 )
 collection = client.get_or_create_collection(name="cafe_collection", embedding_function=openai_ef)
 
-def create_cafe_embedding(cafe_details):
-    attributes = ["name", "formatted_address", "types", "opening_hours", "rating", "price_level", "vicinity"]
-    text = " ".join([str(cafe_details.get(attr, '')) for attr in attributes])
-    embedding = get_embedding(text)
-    return embedding
+def search_cafes_semantically(query, cafe_data, top_k=10):
+    query_embedding = get_embedding(query)
+    similarities = []
 
-def cosine_similarity(a, b):
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+    for cafe in cafe_data:
+
+        cafe_embedding = np.array(collection.get_embedding(cafe["place_id"]))  # Retrieve the list of floats directly
+
+        similarity = np.dot(query_embedding, cafe_embedding) / (np.linalg.norm(query_embedding) * np.linalg.norm(cafe_embedding))
+        similarities.append(similarity)
+
+    top_indices = np.argsort(similarities)[-top_k:][::-1]
+    recommendations = [cafe_data[i] for i in top_indices]
+    return recommendations
+
+def create_cafe_embedding(cafe_details):
+    logging.info(f"Creating embedding for cafe: {cafe_details['name']}")
+    ## dump cafe_details and pretty print it to see what it looks like
+    logging.info(f"Details: {cafe_details}")
+    logging.info(f"ID: {cafe_details['place_id']}")
+
+    attributes = ["name", "formatted_address", "types", "opening_hours", "rating", "price_level", "vicinity"]
+    cafe_details["types"] = ', '.join(cafe_details["types"])  # join the elements in the 'types' list
+    
+    # Convert opening_hours to a string representation
+    if "opening_hours" in cafe_details:
+        cafe_details["opening_hours"] = json.dumps(cafe_details["opening_hours"])
+
+    # Convert reviews to a string representation
+    if "reviews" in cafe_details:
+        cafe_details["reviews"] = json.dumps(cafe_details["reviews"])
+    
+    text = " ".join([str(cafe_details.get(attr, '')) for attr in attributes])
+
+    embedding = openai_ef(text)
+    return list(embedding)  # Convert the embedding into a list of floats
+
 
 def search_cafes(api_key, location, keywords=["cafe", "coffee shop", "brewery", "espresso bar"], limit=10):
     url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
     results = []
 
-    for keyword in keywords:
-        params = {
-            "key": api_key,
-            "query": f"{keyword} in {location}",
-            "maxResults": limit
-        }
-        response = requests.get(url, params=params)
-        results.extend(response.json().get("results", [])[:limit])
+    query = f"{', '.join(keywords)} in {location}"
+    params = {
+        "key": api_key,
+        "query": query,
+        "maxResults": limit,
+        "type": "cafe",
+        "rankby": "prominence"
+    }
+    response = requests.get(url, params=params)
+    results.extend(response.json().get("results", [])[:limit])
 
     return results
 
@@ -56,7 +90,7 @@ def get_cafe_details(api_key, place_id):
         "key": api_key,
         "place_id": place_id,
         "fields": "name,formatted_address,rating,user_ratings_total,types,price_level,website,reviews,"
-                  "opening_hours,url,utc_offset_minutes,vicinity"
+                  "opening_hours,url,vicinity"
     }
     response = requests.get(url, params=params)
     results = response.json()
@@ -66,45 +100,43 @@ def get_cafe_details(api_key, place_id):
 def get_embedding(text: str, model="text-embedding-ada-002") -> list[float]:
     return openai.Embedding.create(input=text, model=model)["data"][0]["embedding"]
 
-def search_cafes_semantically(query, cafe_data, top_k=10):
-    similarities = []
-
-    for cafe in cafe_data:
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=f"Given the query '{query}', how relevant is this cafe: {cafe['name']} at {cafe['formatted_address']}? (1-10)",
-            max_tokens=2,
-            n=1,
-            stop=None,
-            temperature=0.5
-        )
-        score = float(response.choices[0].text.strip())
-        similarities.append(score)
-
-    top_indices = np.argsort(similarities)[-top_k:][::-1]
-    recommendations = [cafe_data[i] for i in top_indices]
-    return recommendations
-
-
 def main():
     logging.info("Searching for cafes in San Francisco...")
     cafes = search_cafes(GOOGLE_MAPS_API_KEY, "San Francisco")
+    logging.info(f"Found {len(cafes)} cafes")
+    ## pretty print the cafes
+    for cafe in cafes:
+        logging.info(f"{cafe['name']} at {cafe['formatted_address']}")
+
     cafe_data = []
 
     for cafe in cafes:
         logging.info(f"Processing cafe: {cafe['name']}")
         details = get_cafe_details(GOOGLE_MAPS_API_KEY, cafe["place_id"])
-        embedding = create_cafe_embedding(details)
-        
-        # Remove embeddings from details before storing as metadata
-        metadata = details.copy()
-        metadata.pop("embedding", None)  # Add a default value of None
+
+        embedding = create_cafe_embedding(details)  # Get the list of floats
         
         cafe_data.append(details)
-        collection.add(embeddings=[embedding], metadatas=[metadata], ids=[cafe["place_id"]])
+
+        logging.info(f"Saving cafe: {cafe['name']} to Chroma collection")
+        logging.info(f"Details: {details}")
+        logging.info(f"ID: {cafe['place_id']}")
+        logging.info(f"Name: {cafe['name']}")
+        logging.info(f"Address: {cafe['formatted_address']}")
+        logging.info(f"Types: {cafe['types']}")
+        logging.info(f"Opening Hours: {cafe.get('opening_hours', 'N/A')}")
+        logging.info(f"Rating: {cafe['rating']}")
+        print("DETAILS ",details)
+        print("PLACE ID",cafe["place_id"])
+        collection.add(embeddings=[embedding], metadatas=[details], ids=[cafe["place_id"]])
 
     logging.info(f"Saved {len(cafe_data)} cafes data to Chroma collection")
 
+    test_query = "cozy cafe with free Wi-Fi"
+    recommendations = search_cafes_semantically(test_query, cafe_data)
+    print("Top recommendations for the query:", test_query)
+    for idx, rec in enumerate(recommendations):
+        print(f"{idx + 1}. {rec['name']} - {rec['formatted_address']}")
+
 if __name__ == "__main__":
     main()
-
