1d34d1f0271c1104dc743cd2d9c2bc1cf9b967fb
Surya <git@sdan.io>
2023-04-27 01:52:27 -0700

    update plugin description

--- cli.py
+++ cli.py
@@ -1,17 +1,31 @@
 # cli.py
 import argparse
-from main import query_cafe_collection
+from main import query_cafe_collection, fetch_and_store_cafe_collection
 
 def main():
     parser = argparse.ArgumentParser(description="Search for cafes in the embeddings database.")
-    parser.add_argument("query", type=str, help="The search query.")
-    parser.add_argument("-n", "--num_results", type=int, default=3, help="Number of results to return.")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # Fetch and store data command
+    fetch_parser = subparsers.add_parser("fetch", help="Fetch and store cafe data for a location")
+    fetch_parser.add_argument("location", type=str, help="Location to fetch and store cafe data")
+
+    # Query command
+    query_parser = subparsers.add_parser("query", help="Query the embeddings database")
+    query_parser.add_argument("query", type=str, help="The search query.")
+    query_parser.add_argument("-n", "--num_results", type=int, default=3, help="Number of results to return.")
+    
     args = parser.parse_args()
 
-    recommendations = query_cafe_collection(args.query, args.num_results)
-    print(f"Top recommendations for the query: {args.query}")
-    for idx, recommendation in enumerate(recommendations, 1):
-        print(f"{idx}. {recommendation['name']}")
+    if args.command == "fetch":
+        fetch_and_store_cafe_collection(args.location)
+    elif args.command == "query":
+        recommendations = query_cafe_collection(args.query, args.num_results)
+        print(f"Top recommendations for the query: {args.query}")
+        for idx, recommendation in enumerate(recommendations, 1):
+            print(f"{idx}. {recommendation['name']}")
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
---
+++ plugin.py
@@ -0,0 +1,67 @@
+import json
+import quart
+import quart_cors
+from quart import request
+import logging
+from process import query_cafe_collection
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+
+@app.post("/recommendations")
+async def recommendations():
+    data = await request.get_json(force=True)
+    query = data["query"]
+    num_results = data.get("num_results", 3)
+
+    cafe_list = query_cafe_collection(query, num_results)
+
+    ## Log recommendations
+    logging.info(f"Recommendations: {cafe_list}\n")
+
+    formatted_recommendations = []
+    for cafe in cafe_list:
+        formatted_str = (f"{cafe['name']} at {cafe['address']} | "
+                         f"About Summary: {cafe['editorial_summary']} | "
+                         f"Types: {cafe['types']} | "
+                         f"Rating: {cafe['rating']} | "
+                         f"Total User Ratings: {cafe['user_ratings_total']} | "
+                         f"Price Level: {cafe['price_level']} | "
+                         f"Opening Hours: {cafe['opening_hours']} | "
+                         f"Reviews: {cafe['reviews']} | "
+                         f"Dine-in: {cafe['dine_in']} | "
+                         f"Delivery: {cafe['delivery']} | "
+                         f"Takeout: {cafe['takeout']}")
+        formatted_recommendations.append(formatted_str)
+
+    print("Formatted Recommendations:", formatted_recommendations)
+
+    return quart.Response(response=json.dumps(formatted_recommendations), status=200)
+
+
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    host = request.headers['Host']
+    with open("ai-plugin.json") as f:
+        text = f.read()
+        text = text.replace("PLUGIN_HOSTNAME", f"http://{host}")
+        return quart.Response(text, mimetype="text/json")
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    host = request.headers['Host']
+    with open("openapi.yaml") as f:
+        text = f.read()
+        text = text.replace("PLUGIN_HOSTNAME", f"http://{host}")
+        return quart.Response(text, mimetype="text/yaml")
+
+def run_plugin():
+    app.run(debug=True, host="0.0.0.0", port=5002)
