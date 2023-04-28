f3b4c5690fde22a6c8d3e34e4b692b6ef7032022
Surya <git@sdan.io>
2023-04-27 21:06:16 -0700

    update port no

--- plugin.py
+++ plugin.py
@@ -81,7 +81,7 @@ async def health_check():
 
 def run_plugin():
     config = Config()
-    config.bind = ["0.0.0.0:8000"]
+    config.bind = ["0.0.0.0:8080"]
     config.access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
     asyncio.run(serve(app, config))
 
